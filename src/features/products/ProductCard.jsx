12ea41eb864d39b0dd56d29d025e448a0c4b9266
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-05-06 16:59:20 +0530

    bug fix

--- src/features/products/ProductCard.jsx
+++ src/features/products/ProductCard.jsx
@@ -1,38 +1,4 @@
-// import React from "react";
-// import { getStockStatus } from "../../utils/stockStatus";
-// import { Link } from "react-router-dom";
 
-
-// // display individual product details in a card format
-// function ProductCard({product,viewMode}){
-//     const dispatch = useDispatch();
-//     const stockStatus = getStockStatus(product.stock);
-//     const stockClass = stockStatus.replaceAll(" ", "-");
-
-//     return(
-//         <div className = {`product-card ${viewMode}`}>
-//            <Link to={`/product/${product.id}`} ClassName="product-link">
-//            <img src={product.image} alt={product.title}/> 
-//         <h3>{product.title}</h3>
-//  </Link>
-//             <div className="product-info">
-//             <p className="category">{product.category}</p>
-//             <p className="price">${product.price.toFixed(2)}</p>
-//             <p className="stock-status">{getStockStatus(product.stock)}</p>
-//             <div className="actions">
-//             {/* <Link to={`/product/${product.id}`}>
-//               <button className="secondary-btn">View Details</button>
-//             </Link> */}
-//             <button disabled={product.stock === 0}>Add to Cart</button>
-//             </div>
-//         </div>
-//     {/* ) */}
-//             </div>
-//         // </div>
-
-//     )
-// }
-// export default ProductCard;
 
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
